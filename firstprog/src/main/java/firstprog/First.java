2f29bd3829b964f9d4da3df23984bb6762d7218c
hp <hp@LAPTOP-RJLSA4M0>
2020-08-27 17:28:51 +0530

    first commit

---
+++ firstprog/src/main/java/firstprog/First.java
@@ -0,0 +1,10 @@
+package firstprog;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("this is first line");
+
+	}
+
+}
---
+++ firstprog/src/test/java/firstprogtest/Test1.java
@@ -0,0 +1,10 @@
+package firstprogtest;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is first test line");
+
+	}
+
+}
